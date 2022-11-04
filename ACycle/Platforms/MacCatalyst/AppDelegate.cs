55d19a196f74118fdd6943a15a3abd58940ab95d
lightyears <lightyears1998@hotmail.com>
2022-11-04 14:07:45 +0800

    Clean up targets

--- ACycle/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace ACycle;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
