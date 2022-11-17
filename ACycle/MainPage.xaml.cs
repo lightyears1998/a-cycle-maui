e225b1b168c350591b0e93edad2f9b0e213b65cf
lightyears <lightyears1998@hotmail.com>
2022-11-17 14:28:50 +0800

    Test out praeclarum/sqlite-net
    SQlite ORM looks so good.

--- ACycle/MainPage.xaml.cs
+++ ACycle/MainPage.xaml.cs
@@ -2,23 +2,15 @@
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+    private int count = 0;
 
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+    public MainPage()
+    {
+        InitializeComponent();
+    }
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
-
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+    private void OnCounterClicked(object sender, EventArgs e)
+    {
+        // 源与流 爱与恨 杯酒与千秋
+    }
 }
-
