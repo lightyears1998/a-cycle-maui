5e18ae2d8fe4e7f0ddec8b9e13b22fcb5b5fb78e
lightyears <lightyears1998@hotmail.com>
2022-11-21 11:36:06 +0800

    Fix the names of tables and columns

--- ACycle/Entities/Entry.cs
+++ ACycle/Entities/Entry.cs
@@ -7,9 +7,14 @@ using System.Threading.Tasks;
 
 namespace ACycle.Entities
 {
+    [Table("entry")]
     public class EntryEntity
     {
         [PrimaryKey]
+        [Column("uuid")]
         public string Uuid { set; get; } = Guid.NewGuid().ToString();
+
+        [Column("removedAt")]
+        public DateTime RemovedAt { set; get; }
     }
 }
