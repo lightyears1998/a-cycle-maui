721a9424b164c802b7d4ef3ca2ceee19df9a3ec4
lightyears <lightyears1998@hotmail.com>
2022-11-23 21:15:31 +0800

    Write some tests

---
+++ ACycleTest/Models/DiaryTest.cs
@@ -0,0 +1,27 @@
+﻿using ACycle.AppServices.Impl;
+using ACycle.EntityRepositories;
+using ACycle.Models;
+
+namespace ACycle.UnitTests.Models
+{
+    [TestClass]
+    public class DiaryTest
+    {
+        [TestMethod]
+        public void Diary_Create()
+        {
+            _ = new Diary();
+        }
+
+        [TestMethod]
+        public async void Diary_Insert()
+        {
+            DatabaseService db = new DatabaseService("TestDatabase.sqlite3");
+            EntryRepository<Diary> repo = new(db);
+
+            Diary diary = new Diary();
+            await db.Initialize();
+            await repo.InsertAsync(diary);
+        }
+    }
+}
