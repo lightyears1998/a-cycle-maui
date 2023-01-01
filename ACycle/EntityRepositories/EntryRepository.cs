29dd824d585e1a5491afa68b32e4649d76b03797
lightyears <lightyears1998@hotmail.com>
2023-01-01 15:15:22 +0800

    Code splitting

--- ACycle/EntityRepositories/EntryRepository.cs
+++ ACycle/EntityRepositories/EntryRepository.cs
@@ -1,7 +1,5 @@
 ﻿using ACycle.AppServices;
 using ACycle.Entities;
-using ACycle.Models;
-using Newtonsoft.Json;
 
 namespace ACycle.EntityRepositories
 {
@@ -39,67 +37,15 @@ namespace ACycle.EntityRepositories
             await _databaseService.MainDatabase.UpdateAsync(entry);
         }
 
+        public async Task DeleteAsync(EntryEntity entry)
+        {
+            await _databaseService.MainDatabase.DeleteAsync(entry);
+        }
+
         public async Task<List<EntryEntity>> FindAllAsync(string contentType)
         {
             var query = _databaseService.MainDatabase.Table<EntryEntity>().Where(entity => entity.ContentType == contentType);
             return await query.ToListAsync();
-        }
-    }
-
-    public class EntryRepository<T>
-        where T : IEntryBasedModel, new()
-    {
-        private readonly EntryRepository _repository;
-
-        public EntryRepository(EntryRepository entryRepository)
-        {
-            _repository = entryRepository;
-        }
-
-        public async Task<T> InsertAsync(T model)
-        {
-            if (model.EntryEntity != null)
-            {
-                throw new ArgumentException("Entry is already in database. Use SaveAsync() or UpdateAsync() instead of InsertAsync().");
-            }
-
-            var entryEntity = new EntryEntity()
-            {
-                ContentType = T.EntryContentType,
-                Content = JsonConvert.SerializeObject(model)
-            };
-
-            await _repository.InsertAsync(entryEntity);
-            model.EntryEntity = entryEntity;
-            return model;
-        }
-
-        public async Task<T> UpdateAsync(T model)
-        {
-            if (model.EntryEntity == null)
-            {
-                throw new ArgumentException("Entry is not in database. Use SaveAsync() to save the entity into database first.");
-            }
-
-            model.EntryEntity.Content = JsonConvert.SerializeObject(model);
-            await _repository.UpdateAsync(model.EntryEntity);
-            return model;
-        }
-
-        public async Task<List<T>> FindAllAsync()
-        {
-            var entities = await _repository.FindAllAsync(T.EntryContentType);
-            var objects = new List<T>();
-            foreach (var entity in entities)
-            {
-                var obj = JsonConvert.DeserializeObject<T>(entity.Content);
-                if (obj != null)
-                {
-                    obj.EntryEntity = entity;
-                    objects.Add(obj);
-                }
-            }
-            return objects;
         }
     }
 }
---
+++ ACycle/EntityRepositories/EntryRepositoryOfT.cs
@@ -0,0 +1,73 @@
+﻿using ACycle.Entities;
+using ACycle.Models;
+using Newtonsoft.Json;
+
+namespace ACycle.EntityRepositories
+{
+    public class EntryRepository<T>
+        where T : IEntryBasedModel, new()
+    {
+        private readonly EntryRepository _rawRepository;
+
+        public EntryRepository(EntryRepository entryRepository)
+        {
+            _rawRepository = entryRepository;
+        }
+
+        public async Task<T> InsertAsync(T model)
+        {
+            if (model.EntryEntity != null)
+            {
+                throw new ArgumentException("Entry is already in database. Use SaveAsync() or UpdateAsync() instead of InsertAsync().");
+            }
+
+            var entryEntity = new EntryEntity()
+            {
+                ContentType = T.EntryContentType,
+                Content = JsonConvert.SerializeObject(model)
+            };
+
+            await _rawRepository.InsertAsync(entryEntity);
+            model.EntryEntity = entryEntity;
+            return model;
+        }
+
+        public async Task<T> UpdateAsync(T model)
+        {
+            if (model.EntryEntity == null)
+            {
+                throw new ArgumentException("Entry is not in database. Use SaveAsync() to save the entity into database first.");
+            }
+
+            model.EntryEntity.Content = JsonConvert.SerializeObject(model);
+            await _rawRepository.UpdateAsync(model.EntryEntity);
+            return model;
+        }
+
+        public async Task DeleteAsync(T model)
+        {
+            if (model.EntryEntity == null)
+            {
+                throw new ArgumentException("Entry is not in database, thus it is not able to delete it from database.");
+            }
+
+            await _rawRepository.DeleteAsync(model.EntryEntity);
+        }
+
+        public async Task<List<T>> FindAllAsync()
+        {
+            var entities = await _rawRepository.FindAllAsync(T.EntryContentType);
+            var objects = new List<T>();
+            foreach (var entity in entities)
+            {
+                var obj = JsonConvert.DeserializeObject<T>(entity.Content);
+                if (obj != null)
+                {
+                    obj.EntryEntity = entity;
+                    objects.Add(obj);
+                }
+            }
+            return objects;
+        }
+    }
+}
