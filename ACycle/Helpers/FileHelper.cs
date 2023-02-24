8c7e2ebede62bd754ba39444ce61b9d4b40a95a1
lightyears <lightyears1998@hotmail.com>
2023-02-24 16:52:39 +0800

    Add script to process files

---
+++ ACycle/Helpers/FileHelper.cs
@@ -0,0 +1,15 @@
+﻿namespace ACycle.Helpers
+{
+    public static class FileHelper
+    {
+        public static async Task CopyAsync(string sourceFilePath, string destinationFilePath, bool overWrite = false)
+        {
+            const int bufferSize = 4096;
+            FileMode writeMode = overWrite ? FileMode.Create : FileMode.CreateNew;
+
+            using var sourceStream = new FileStream(sourceFilePath, FileMode.Open, FileAccess.Read, FileShare.Read, bufferSize, FileOptions.Asynchronous | FileOptions.SequentialScan);
+            using var destinationStream = new FileStream(destinationFilePath, writeMode, FileAccess.Write, FileShare.None, bufferSize, FileOptions.Asynchronous | FileOptions.SequentialScan);
+            await sourceStream.CopyToAsync(destinationStream);
+        }
+    }
+}
