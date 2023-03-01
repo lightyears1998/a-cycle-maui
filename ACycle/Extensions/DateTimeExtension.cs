c48a5d5c2733c081036569fa15b64349fab0914e
lightyears <lightyears1998@hotmail.com>
2023-03-01 20:25:20 +0800

    Add method for trimming DateTime

---
+++ ACycle/Extensions/DateTimeExtension.cs
@@ -0,0 +1,15 @@
+﻿namespace ACycle.Extensions
+{
+    public static class DateTimeExtension
+    {
+        public static DateTime Trim(this DateTime dateTime, long roundTicks)
+        {
+            return new DateTime(dateTime.Ticks - dateTime.Ticks % roundTicks, dateTime.Kind);
+        }
+
+        public static DateTime TrimSeconds(this DateTime dateTime)
+        {
+            return dateTime.Trim(TimeSpan.TicksPerSecond);
+        }
+    }
+}
