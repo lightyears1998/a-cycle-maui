90bdf15b05ba8397298372145ccaebe1a6671543
lightyears <lightyears1998@hotmail.com>
2023-03-02 15:15:08 +0800

    Rework sync config

--- ACycle/Entities/User/SynchronizationConfigV1.cs
+++ ACycle/Entities/User/SynchronizationConfigV1.cs
@@ -1,6 +1,36 @@
-﻿namespace ACycle.Entities
+﻿using SQLite;
+
+namespace ACycle.Entities
 {
+    [Table("user_synchronization_config")]
     public class SynchronizationConfigV1
     {
+        [PrimaryKey]
+        [Column("id")]
+        public long Id { get; set; }
+
+        [Column("host")]
+        public string Host { get; set; } = string.Empty;
+
+        [Column("path")]
+        public string Path { set; get; } = string.Empty;
+
+        [Column("http_port")]
+        public int HttpPort { set; get; } = 443;
+
+        [Column("ws_port")]
+        public int WsPort { set; get; } = 44;
+
+        [Column("username")]
+        public string Username { set; get; } = "guest";
+
+        [Column("password_sha256")]
+        public string PasswordSha256 { set; get; } = string.Empty;
+
+        [Column("use_tls")]
+        public bool UseTLS { set; get; } = true;
+
+        [Column("is_enabled")]
+        public bool IsEnabled { set; get; } = true;
     }
 }
