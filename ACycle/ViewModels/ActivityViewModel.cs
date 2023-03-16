71e9856c63c544d066ecaa3d8451334c3090cb62
lightyears <lightyears1998@hotmail.com>
2023-03-16 10:42:16 +0800

    Quick fix for #16

--- ACycle/ViewModels/ActivityViewModel.cs
+++ ACycle/ViewModels/ActivityViewModel.cs
@@ -46,11 +46,13 @@ namespace ACycle.ViewModels
             _navigationService = navigation;
             _dialogService = dialogService;
 
-            _activities = new RelayCollection<Activity, ActivityRelay>((item, _) => new ActivityRelay(
-                item: item,
-                editCommand: new AsyncRelayCommand(async () => await EditActivityAsync(item)),
-                removeCommand: new AsyncRelayCommand(async () => await RemoveActivityAsync(item))
-            ));
+            _activities = new RelayCollection<Activity, ActivityRelay>((item, relay) => new ActivityRelay(item, relay =>
+            {
+                return (
+                    new RelayCommand(() => _ = EditActivityAsync(relay.Item)),
+                    new RelayCommand(() => _ = RemoveActivityAsync(relay.Item))
+                    );
+            }));
         }
 
         [RelayCommand]
@@ -138,16 +140,17 @@ namespace ACycle.ViewModels
 
         public class ActivityRelay : Relay<Activity>
         {
-            public ICommand EditCommand { get; protected set; }
+            public ICommand EditCommand { get; set; }
 
-            public ICommand RemoveCommand { get; protected set; }
+            public ICommand RemoveCommand { get; set; }
 
             public string DateTimeString => $"{Item.StartDateTime} => {Item.EndDateTime} ({TimeSpan.FromTicks(Item.EndDateTime.Ticks - Item.StartDateTime.Ticks).TotalHours:0.##}hr)";
 
-            public ActivityRelay(Activity item, ICommand editCommand, ICommand removeCommand) : base(item)
+            public ActivityRelay(Activity item, Func<ActivityRelay, (ICommand EditCommand, ICommand RemoveCommand)> commandBuilder) : base(item)
             {
-                EditCommand = editCommand;
-                RemoveCommand = removeCommand;
+                var commands = commandBuilder(this);
+                EditCommand = commands.EditCommand;
+                RemoveCommand = commands.RemoveCommand;
             }
         }
     }
